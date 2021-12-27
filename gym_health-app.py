1e571b028ef64a4b70f431a2228c21bfbc4d47c7
tayyabmalik4 <mtayyabmalik99@gmail.com>
2021-12-27 08:12:35 +0500

    1.0

--- gym_health-app.py
+++ gym_health-app.py
@@ -2,8 +2,8 @@ import datetime
 def getdate():
     return datetime.datetime.now()
 
-print("Welcome to my health jim")
-choose = int(input("1 for write and 2 for read: "))
+print("Welcome to my health jim:")
+choose = int(input("1 for write and 2 for read: \n"))
 # if choose==1:
     # choose11=int(input("Enter 1 for food and 2 for exercise"))
     # reading(choose11)
@@ -14,58 +14,58 @@ choose = int(input("1 for write and 2 for read: "))
 if choose==1:
     def writing(w):
         if w==1:
-            print("Welcome to diet plane")
-            c = int(input("Enter 1 for Tayyab 2 for junaid and 3 for zahid: "))
+            print("Welcome to diet plane: ")
+            c = int(input("Enter 1 for Tayyab 2 for junaid and 3 for zahid: \n"))
             
             if c==1:
-                value = input("Please enter the food: ")
+                value = input("Please enter the food: \n")
                 f1= open("tayyab-food.txt","a")
                 f1.write(str([str(getdate())])+":"+value+"\n")
                 f1.close()
             elif c==2:
-                value = input("Please enter the food: ")
+                value = input("Please enter the food: \n")
                 f1=open("junaid-food.txt","a")
                 f1.write(str([str(getdate())])+":"+value+"\n")
                 f1.close()
             elif c==3:
-                value = input("Please enter the food: ")
+                value = input("Please enter the food: \n")
                 f1 = open("zahid-food.txt","a")
                 f1.write(str([str(getdate())])+":"+value+"\n")
                 f1.close()
-            print("Thanks to add the food")
+            print("Thanks to add the food!")
         
         elif w==2:
             print("Welcome to excercise plane")
-            c = int(input("Enter 1 for Tayyab 2 for junaid and 3 for zahid: "))
+            c = int(input("Enter 1 for Tayyab 2 for junaid and 3 for zahid: \n"))
             if c==1:
-                value = input("Please enter the excercise plane: ")
+                value = input("Please enter the excercise plane: \n")
                 f1= open("tayyab-exe.txt","a")
                 f1.write(str([str(getdate())])+":"+value+"\n")
                 f1.close()
             elif c==2:
-                value = input("Please enter the excercise plane: ")
+                value = input("Please enter the excercise plane: \n")
                 f1=open("junaid-exe.txt","a")
                 f1.write(str([str(getdate())])+":"+value+"\n")
                 f1.close()
             elif c==3:
-                value = input("Please enter the excercise plane: ")
+                value = input("Please enter the excercise plane: \n")
                 f1 = open("zahid-exe.txt","a")
                 f1.write(str([str(getdate())])+":"+value+"\n")
                 f1.close()
-            print("Thanks to add the exercise")
-        print("if you run again then press 1 and for exit press any key")
-        key = input()
+            print("Thanks to add the exercise!")
+        print("if you run again then press 1 and for exit press any key: \n")
+        key = int(input())
         if key==1:
             writing(w)
         else:
-            print("thanks for adding the values")
-    w=int(input("Enter 1 for food and 2 for exercise"))
+            print("thanks for adding the values!")
+    w=int(input("Enter 1 for food and 2 for exercise: "))
     writing(w)
 else:
     def reading(re):
         if re==1:
-            print("Welcome   to food reading")
-            c=int(input("Enter 1 for Tayyab and 2 for Junaid and 3 for zahid"))
+            print("Welcome   to food reading: ")
+            c=int(input("Enter 1 for Tayyab and 2 for Junaid and 3 for zahid: \n"))
             if c==1:
                 f1=open("tayyab-food.txt")
                 content=f1.read()
@@ -82,8 +82,8 @@ else:
                 print(content)
                 f1.close()
         elif re==2:
-            print("Welcome to excercise reading")
-            c=int(input("Enter 1 for Tayyab and 2 for Junaid and 3 for zahid"))
+            print("Welcome to excercise reading: ")
+            c=int(input("Enter 1 for Tayyab and 2 for Junaid and 3 for zahid: \n"))
             if c==1:
                 f1=open("tayyab-exe.txt")
                 content=f1.read()
@@ -99,12 +99,12 @@ else:
                 content=f1.read()
                 print(content)
                 f1.close()
-        print("if you run again then press 1 and for exit press any key")
-        key=input()
+        print("if you run again then press 1 and for exit press any key: \n")
+        key=int(input())
         if key==1:
-            reading()
+            reading(re)
         else:
             print("Thanks for using this app")
-    re=int(input("Enter 1 for food and 2 for exercise"))
+    re=int(input("Enter 1 for food and 2 for exercise: \n"))
     reading(re)            
 
